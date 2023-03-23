259d41b14ad712432d6b51f946a7db643e1ede79
Leonardo <127408233+VenakBaur@users.noreply.github.com>
2023-03-22 21:50:22 -0300

    Aula 22/03 portugol

---
+++ Aula-22-03/src/aula/pkg22/pkg03/Sequencia_Escape2.java
@@ -0,0 +1,16 @@
+package aula.pkg22.pkg03;
+
+public class Sequencia_Escape2 {
+    
+    public static void main(String[] args) {
+        
+        String nome = "Marcela";
+        int idade = 15;
+        String cidade = "Santos";
+        char sexo = 'f';
+        float salario = 1450.00f;
+        
+        System.out.printf("Nome: %s\nIdade: %d\nmora em: %s\nSexo: %c\nSalario: %.2f \n",nome,idade,cidade,sexo,salario);
+        
+    }
+}
